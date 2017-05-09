47981377cb1b10a40f4709e073c0c3238b39de97
Arthorn Kittinukul <joker.arthorn@gmail.com>
2017-05-09 13:17:16 +0700

    Adding edit user profile page
    adding edit user profile page and update controller

--- BitMe/BitMe/Controllers/HomeController.cs
+++ BitMe/BitMe/Controllers/HomeController.cs
@@ -3,20 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BitMe.Models;
 
 namespace BitMe.Controllers
 {
     public class HomeController : Controller
     {
+
+        User user = new Models.User();
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
-
+        [HttpGet]
         public ActionResult Profile()
         {
-            return View();
+            //Connect to db and add each data to model to display.
+            //such as model.UName = db.Uname;
+            User model = new User();
+            model.UName = "iJOKE";
+            model.UAccountID = 12345;
+            model.UPassword = "12345*";
+            model.UEmail = "Email@gmail";
+            model.UMoney = 1000;
+            return View(model);
         }
+        [HttpGet]
+        public ActionResult EditProfile()
+        {
+            //Connect to db and add each data to model to display.
+            //such as model.UName = db.Uname;
+            User model = new User();
+            model.UName = "iJOKE";
+            model.UAccountID = 12345;
+            model.UPassword = "12345*";
+            model.UEmail = "Email@gmail";
+            model.UMoney = 1000;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditProfile(User userProfile)
+        {
+            if (ModelState.IsValid)
+            {
+                user.UName = userProfile.UName;
+                user.UAccountID = userProfile.UAccountID;
+                user.UPassword = userProfile.UPassword;
+                user.UEmail = userProfile.UEmail;
+                return RedirectToAction("Profile", "Home");
+            }
+            return View(userProfile);
+        }
+
+
+
     }
 }
