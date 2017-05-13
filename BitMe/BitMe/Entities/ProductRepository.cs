3d8e3ec3fae7cf979733cf51f6a853c7303def6d
kanomtang <rungroj_s@cmu.ac.th>
2017-05-13 18:22:01 +0700

    update

--- BitMe/BitMe/Entities/ProductRepository.cs
+++ BitMe/BitMe/Entities/ProductRepository.cs
@@ -4,11 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BitMe.Models;
+using System.Web;
+using System.Drawing;
 
 namespace BitMe.Entities
 {
     interface ProductRepository
     {
-        IQueryable<Item> Products { get; }
+        //BidMeDatabaseEntities1 db = new BidMeDatabaseEntities1();
+        //above line for create instace of database 
+
+
+        public void addItem(Item i, HttpPostedFileBase p)
+        {
+            var a = i.ProductName;
+            var b = ConvertToBytes(p);
+            //db.AddAP(a, b);
+            //above line for adding product to database
+        }
+
+
+        public byte[] ConvertToBytes(HttpPostedFileBase image)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(image, typeof(byte[]));
+        }
+
     }
 }
